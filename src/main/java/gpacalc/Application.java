dd7f32e624ecd88c03c0fba31aa0f7fcbd9cd301
Vinci Lee <davinlsj@gmail.com>
2024-01-07 20:53:55 +0900

    테스트2

--- src/main/java/gpacalc/Application.java
+++ src/main/java/gpacalc/Application.java
@@ -3,6 +3,6 @@ package gpacalc;
 public class Application {
     public static void main(String[] args) {
         //TODO: 구현
-        System.out.println("Test");
+        System.out.println("Test2");
     }
 }
