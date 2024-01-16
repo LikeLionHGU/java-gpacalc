d3354f33282c58a03de7b0989510dd482790211b
7SH7 <kimseunghwan7777@gmail.com>
2024-01-17 00:36:19 +0900

    finish day2 assign

--- src/main/java/gpacalc/Method.java
+++ src/main/java/gpacalc/Method.java
@@ -3,12 +3,10 @@ package gpacalc;
 public class Method {
 	CheckException checking = new CheckException();
 
-	public void categorize(String input, String major_liberal) {
-		String[] first_split = input.split(",");
+	public void categorize(String subjects, String major_liberal) {
+		String[] first_split = subjects.split(",");
 		String[][] second_split = new String[first_split.length][3];
-		for (int i = 0; i < first_split.length; i++) {
-			second_split[i] = first_split[i].split("-");
-		}
+		for (int i = 0; i < first_split.length; i++)	second_split[i] = first_split[i].split("-");
 
 		for (int i = 0; i < first_split.length; i++) {
 			checking.checkSubjectLength(second_split[i][0]);
@@ -28,6 +26,7 @@ public class Method {
 		String[] first_split = input.split(",");
 		String[][] second_split = new String[first_split.length][3];
 		for (int i = 0; i < first_split.length; i++) second_split[i] = first_split[i].split("-");
+
 		for (String[] strings : second_split) {
 			int subject_credit = Integer.parseInt(strings[1]);
 			checking.checkCreditRange(subject_credit);
@@ -38,30 +37,25 @@ public class Method {
 		return credit;
 	}
 
-	// /// - 1 - \\\  랑 그 밑이랑 하나씩 새로운 mehtod를 사용하기!
-	public double calculate(String all, int credit){
-		///// - 1 - \\\\\\\\\
-		// A+(4.5), A0(4.0), B+(3.5), B0(3.0), C+(2.5), C0(2.0), D+(1.5), D0(1.0), F(0)
-		String[] first_split = all.split(",");
+	public static double calculate(String subjects, int credit){
+		String[] first_split = subjects.split(",");
 		String[][] second_split = new String[first_split.length][3];
-		for(int i = 0 ; i < first_split.length ; i++){
-			second_split[i] = first_split[i].split("-");
+		for(int i = 0 ; i < first_split.length ; i++) 	second_split[i] = first_split[i].split("-");
+
+		double sum = 0;
+		for(int i = 0 ; i < first_split.length ; i++) {
 			if(second_split[i][2].equals("P")) credit -= Integer.parseInt(second_split[i][1]);
 			if(second_split[i][2].equals("F")) credit += Integer.parseInt(second_split[i][1]);
-		}
-
-		//// - 2 - \\\\\\\\
-		double sum = 0;
-		for(int i = 0 ;i < first_split.length ; i++){
 			sum += (Double.parseDouble(second_split[i][1]) * Matching(second_split[i][2]));
 		}
+
 		double result = sum / credit;
-		result = Math.round(result*100)/(double)100 ;
+		result = Math.round(result*100) / (double)100 ;
 
 		return result;
 	}
 
-	public double Matching(String grade){
+	public static double Matching(String grade){
 		double score = 0;
 		switch(grade){
 			case "A+" -> score = 4.5;
@@ -72,7 +66,10 @@ public class Method {
 			case "C0" -> score = 2.0;
 			case "D+" -> score = 1.5;
 			case "D0" -> score = 1.0;
-			default -> score = 0;
+			case "F" -> score = 0;
+			case "P" -> score = 0;
+			case "NP" -> score = 0;
+			default -> throw new IllegalArgumentException("잘못된 입력입니다.");
 		}
 		return score;
 	}
